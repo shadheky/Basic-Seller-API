d022b94cfa4b3cc1deb96a90fafee375d0875997
shadheky <92411362+shadheky@users.noreply.github.com>
2022-08-16 09:34:22 -0300

    Camada de serviços e componente de registro

--- src/main/java/com/micron/Course/resources/UserResource.java
+++ src/main/java/com/micron/Course/resources/UserResource.java
@@ -1,20 +1,35 @@
 package com.micron.Course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.micron.Course.entities.User;
+import com.micron.Course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+	@Autowired
+	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L,"Maria", "maria@gmail.com", "3124512", "123456789");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		
+		
+		return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable  Long id){	
+		return ResponseEntity.ok().body(service.findById(id));
+	}
+	
 	
 }
---
+++ src/main/java/com/micron/Course/services/UserService.java
@@ -0,0 +1,25 @@
+package com.micron.Course.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.micron.Course.entities.User;
+import com.micron.Course.repositories.UserRepository;
+
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long ID) {
+		return repository.findById(ID).get();
+	}
+}
